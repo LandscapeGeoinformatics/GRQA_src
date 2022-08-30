d2a73cdb4151ba249ac98691f2c9f18485592be5
holgervirro <78081374+holgervirro@users.noreply.github.com>
2022-08-30 11:24:40 +0300

    Update GLORICH preprocessing scripts for GRQA_v1.3

--- preprocessing/GLORICH/glorich_preprocessing.py
+++ preprocessing/GLORICH/glorich_preprocessing.py
@@ -89,15 +89,21 @@ def get_stats_df(df, groupby_cols, value_col, date_col):
 ds_name = 'GLORICH'
 
 # Directory paths
-proj_dir = '/gpfs/space/home/holgerv/gis_holgerv/river_quality'
-raw_dir = os.path.join(proj_dir, 'data', ds_name, 'raw')
-proc_dir = os.path.join(proj_dir, 'data', ds_name, 'processed')
+# proj_dir = '/gpfs/space/home/holgerv/gis_holgerv/river_quality'
+# raw_dir = os.path.join(proj_dir, 'data', ds_name, 'raw')
+# proc_dir = os.path.join(proj_dir, 'data', ds_name, 'processed')
+
+# Directory paths
+proj_dir = '/gpfs/terra/export/samba/gis/holgerv'
+raw_dir = os.path.join(proj_dir, 'river_quality', 'data', ds_name, 'raw')
+proc_dir = os.path.join(proj_dir, 'GRQA_v1.3', 'GRQA_source_data', ds_name, 'processed')
 
 # Download directory
 dl_dir = os.path.join(raw_dir, 'download_2020-11-16')
 
 # Import the code map
-cmap_file = os.path.join(raw_dir, 'meta', ds_name + '_code_map.csv')
+# cmap_file = os.path.join(raw_dir, 'meta', ds_name + '_code_map.csv')
+cmap_file = os.path.join(proj_dir, 'GRQA_v1.3', 'GRQA_source_data', ds_name, 'raw', 'meta', ds_name + '_code_map.csv')
 cmap_df = pd.read_csv(cmap_file, sep=';')
 param_codes = cmap_df['source_param_code'].to_list()
 
--- preprocessing/GLORICH/glorich_preprocessing.sh
+++ preprocessing/GLORICH/glorich_preprocessing.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-#SBATCH -p main
+#SBATCH -p amd
 #SBATCH -J glorich_preprocessing
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH -t 01:00:00
 #SBATCH --mem=64G
 
-cd /gpfs/space/home/holgerv/gis_holgerv/river_quality/scripts/preprocessing/GLORICH
+# Name of the dataset
+ds_name="GLORICH"
+
+# cd /gpfs/space/home/holgerv/gis_holgerv/river_quality/scripts/preprocessing/GLORICH
+cd /gpfs/terra/export/samba/gis/holgerv/river_quality/scripts/preprocessing/${ds_name}
 
 module purge
-module load python-3.7.1
+# module load python-3.7.1
+module load python
 
 source activate river_quality
 ~/.conda/envs/river_quality/bin/python glorich_preprocessing.py
