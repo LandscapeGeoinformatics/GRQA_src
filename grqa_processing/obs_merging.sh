aab6f19783f840f2729a7075ee1dfdcbfd97757e
holgervirro <78081374+holgervirro@users.noreply.github.com>
2022-09-01 09:31:37 +0300

    Add files via upload

--- grqa_processing/obs_merging.sh
+++ grqa_processing/obs_merging.sh
@@ -2,10 +2,11 @@
 
 #SBATCH -p amd
 #SBATCH -J obs_merging
-#SBATCH -N 1
+#SBATCH -N 4
 #SBATCH --ntasks-per-node=1
-#SBATCH -t 20:00:00
+#SBATCH -t 10:00:00
 #SBATCH --mem=256G
+#SBATCH --array=0-44
 
 # cd /gpfs/space/home/holgerv/gis_holgerv/river_quality/scripts/grqa_processing
 cd /gpfs/terra/export/samba/gis/holgerv/river_quality/scripts/grqa_processing
@@ -17,9 +18,8 @@ module load python
 source activate river_quality
 
 # code_file="/gpfs/space/home/holgerv/gis_holgerv/river_quality/data/GRQA/meta/GRQA_param_codes.txt"
-# code_file="/gpfs/terra/export/samba/gis/holgerv/GRQA_v1.3/GRQA_meta/GRQA_param_codes.txt"
-# readarray param_codes < ${code_file}
-# param_code=${param_codes[$SLURM_ARRAY_TASK_ID]}
-param_code="TEMP"
+code_file="/gpfs/terra/export/samba/gis/holgerv/GRQA_v1.3/GRQA_meta/GRQA_param_codes.txt"
+readarray param_codes < ${code_file}
+param_code=${param_codes[$SLURM_ARRAY_TASK_ID]}
 
 ~/.conda/envs/river_quality/bin/python obs_merging.py ${param_code}
