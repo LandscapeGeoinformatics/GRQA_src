cdcee96f90778aeccef8b565a90b58e8770d2bde
Holger Virro <holgerv@ut.ee>
2025-05-02 14:01:27 +0300

    Update data catalog script for version 1.4 (#5)

--- grqa_processing/create_data_catalog.py
+++ grqa_processing/create_data_catalog.py
@@ -1,26 +1,31 @@
 # Import libraries
+import sys
 import os
 import glob
 import re
 
 import pandas as pd
-
 from docx import Document
 from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT, WD_LINE_SPACING
 from docx.shared import Inches, Pt, RGBColor
 
-# Data directory
-# data_dir = 'D:/river_quality/data'
+# Project directory
+proj_dir = sys.argv[1]
 
-version = 1.3
-proj_dir = fr'\\export.hpc.ut.ee\gis\holgerv\GRQA_v{version}'
-data_dir = os.path.join(proj_dir, f'GRQA_data_v{version}')
-meta_dir = os.path.join(proj_dir, 'GRQA_meta')
-fig_dir = os.path.join(proj_dir, 'GRQA_figures')
+# Dataset version
+version = sys.argv[2]
+
+# Data directory
+data_dir  = os.path.join(proj_dir, 'final', 'GRQA_data')
+
+# Metadata directory
+meta_dir  = os.path.join(proj_dir, 'final', 'GRQA_meta')
+
+# Figure directory
+fig_dir  = os.path.join(proj_dir, 'final', 'GRQA_figures')
 
 # Parameter codes and names
-# param_stats = pd.read_csv(os.path.join(data_dir, 'GRQA/meta/GRQA_param_stats.csv'), sep=';')
 param_stats = pd.read_csv(os.path.join(meta_dir, 'stats', 'GRQA_param_stats.csv'), sep=';')
 param_codes = list(param_stats['Parameter code'])
 param_names = list(param_stats['Parameter name'])
@@ -66,7 +71,6 @@ font.size = Pt(12)
 font.color.rgb = RGBColor.from_string('000000')
 
 # Add title
-# document.add_paragraph('Data catalog of Global River Water Quality Archive (GRQA)', style='New Title')
 title = f'Data catalog of Global River Water Quality Archive (GRQA) v{version}'
 document.add_paragraph(title, style='New Title')
 paragraph = document.paragraphs[-1]
@@ -78,7 +82,6 @@ width = Inches(5.9527559055)
 # Add all figures to the Word document with corresponding headings
 for param_code in param_codes:
     document.add_paragraph('{} ({})'.format(param_dict[param_code], param_code), style='New Heading 1')
-    # figures = glob.glob(os.path.join(data_dir, 'GRQA/figures/{}_GRQA*.png'.format(param_code)))
     figures = glob.glob(os.path.join(fig_dir, '{}_GRQA*.png'.format(param_code)))
     for fig in figures:
         string = re.search(r'{}_GRQA_(.*?).png'.format(param_code), fig).group(1)
@@ -90,5 +93,12 @@ for param_code in param_codes:
     paragraph_format = paragraph.paragraph_format
     paragraph_format.line_spacing_rule = WD_LINE_SPACING.ONE_POINT_FIVE
 
-# Save document
-document.save(os.path.join(proj_dir, f'GRQA_data_catalog_v{version}.docx'))
+# Save as Word document without table of contents (TOC)
+fp_docx = os.path.join(
+    proj_dir, 'working', f'GRQA_data_catalog_v{version}_without_toc.docx'
+)
+if os.path.exists(fp_docx):
+    os.remove(fp_docx)
+document.save(fp_docx)
+
+# TOC has to be added in Word before saving as final PDF
